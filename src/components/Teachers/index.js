da7de586c2a8ec277ff3ef5c424e901fdf94db79
Radoslav Georgiev <radorado@hacksoft.io>
2019-01-17 11:30:12 +0200

    Add Rosi as teacher

--- src/components/Teachers/index.js
+++ src/components/Teachers/index.js
@@ -1,12 +1,23 @@
 import React from 'react'
 
 import styles from './styles.module.css'
+import rosi from './rosi.jpg'
 import radoslav from './radoslav.jpg'
-import ivaylo from './ivaylo.jpg'
 
 export default () => {
   return (
     <div className={styles.Container}>
+      <div className={styles.TeacherContainer}>
+        <img className={styles.TeacherImage} src={rosi} alt="Ivaylo" />
+        <div className={styles.TeacherInfo}>
+          <p className={styles.TeacherName}>Rositsa Zlateva</p>
+          <ul>
+            <li>Software Developer at HedgeServ.</li>
+            <li>Info point here.</li>
+            <li>Info point here.</li>
+          </ul>
+        </div>
+      </div>
       <div className={styles.TeacherContainer}>
         <img className={styles.TeacherImage} src={radoslav} alt="Radoslav" />
         <div className={styles.TeacherInfo}>
@@ -21,17 +32,6 @@ export default () => {
               6 years of experience teaching different courses, including
               Python, JavaScript and Haskell
             </li>
-          </ul>
-        </div>
-      </div>
-      <div className={styles.TeacherContainer}>
-        <img className={styles.TeacherImage} src={ivaylo} alt="Ivaylo" />
-        <div className={styles.TeacherInfo}>
-          <p className={styles.TeacherName}>Ivaylo Bachvarov</p>
-          <ul>
-            <li>CTO at HackSoft.</li>
-            <li>Using React everday in a big single page project.</li>
-            <li>Loves teaching and doing it for the last 4 years.</li>
           </ul>
         </div>
       </div>
