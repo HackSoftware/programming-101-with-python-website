0d14d4d82d107a0a181bfe9f605df310c6cc9dfb
Radoslav Georgiev <radorado@hacksoft.io>
2019-01-28 11:45:53 +0200

    Update bio to match Python skills

--- src/components/Teachers/index.js
+++ src/components/Teachers/index.js
@@ -24,13 +24,13 @@ export default () => {
           <p className={styles.TeacherName}>Radoslav Georgiev</p>
           <ul>
             <li>CEO at HackSoft.</li>
-            <li>Using React everday in a big single page project.</li>
+            <li>Using Python and Django everday in a big single page project.</li>
             <li>
-              8 years of experience with JavaScript and frontend technologies
+              8 years of experience with Python and backend technologies.
             </li>
             <li>
               6 years of experience teaching different courses, including
-              Python, JavaScript and Haskell
+              Python, JavaScript and Haskell.
             </li>
           </ul>
         </div>
