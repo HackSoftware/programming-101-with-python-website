45e1b6f6e1c0dcfa67aae65f965cf25f9ec7067d
Radoslav Georgiev <radorado@hacksoft.io>
2019-03-20 14:22:31 +0200

    Add `Acronis` and `HedgeServ` as partners

--- src/components/Partners/index.js
+++ src/components/Partners/index.js
@@ -5,6 +5,8 @@ import styles from './styles.module.css'
 import nemetschek from './images/nemetschek.jpg'
 import industria from './images/industria.png'
 import hacksoft from './images/hacksoft.png'
+import hedgeserv from './images/hedgeserv.png'
+import acronis from './images/acronis.png'
 
 export default () => {
   return (
@@ -17,6 +19,12 @@ export default () => {
       </a>
       <a href="https://www.hacksoft.io/">
         <img src={hacksoft} alt="HackSoft" />
+      </a>
+      <a href="https://www.hedgeserv.com/">
+        <img src={hedgeserv} alt="HedgeServ" />
+      </a>
+      <a href="https://www.acronis.com/en-eu/">
+        <img src={acronis} alt="Acronis" />
       </a>
     </div>
   )
